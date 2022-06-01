8c7322113fb5d536bc86b711e5dd4a1d956aa714
Amon I <101267106+horseking82@users.noreply.github.com>
2022-06-01 16:17:14 +0200

    video fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,7 +46,7 @@ const ItuskiHome = () => {
     return (
         <>
             <div className='d-flex justify-content-center align-items-center'>
-                <video width="100%" height="1080px" autoPlay muted> 
+                <video width="100%" height="1080px" autoPlay muted loop> 
                     <source src='/video/itsuki-braches.mp4' type="video/mp4" />
                 </video>
             </div>
